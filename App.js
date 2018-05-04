a2d030ef2e2df4bf8e55591d4221b0766f1052d4
fwchen <crystal.tyan@gmail.com>
2018-05-04 08:43:08 +0800

    update

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
-        <Button>antd-mobile button</Button>
+        <Button type="warning">antd-mobile button</Button>
       </View>
     );
   }
