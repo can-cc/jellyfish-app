c359ec7e5217e33dabfd8014e57e409153adb85c
fwchen <crystal.tyan@gmail.com>
2018-07-11 22:53:42 +0800

    update

--- src/component/todo/TodoCreater.component.js
+++ src/component/todo/TodoCreater.component.js
@@ -1,15 +1,27 @@
 // @flow
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
-import { Button, List, Checkbox, InputItem, WhiteSpace, Flex, Modal } from 'antd-mobile-rn';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image } from 'react-native';
+import {
+  Button,
+  TextareaItem,
+  List,
+  Checkbox,
+  InputItem,
+  WhiteSpace,
+  Flex,
+  Modal
+} from 'antd-mobile-rn';
+import DateTimePicker from 'react-native-modal-datetime-picker';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+const Item = List.Item;
+import format from 'date-fns/format';
 
 import { createForm } from 'rc-form';
 
 class TodoCreaterCompoent extends React.Component<{
   onSubmit: any
 }> {
-  state = { content: '', value: '' };
+  state = { content: '', value: '', deadline: null, isDateTimePickerVisible: false };
 
   submit = () => {
     this.setState({ content: this.state.value });
@@ -25,6 +37,15 @@ class TodoCreaterCompoent extends React.Component<{
     });
   };
 
+  showDateTimePicker = () => this.setState({ isDateTimePickerVisible: true });
+
+  hideDateTimePicker = () => this.setState({ isDateTimePickerVisible: false });
+
+  handleDatePicked = date => {
+    this.setState({ deadline: date.getTime() });
+    this.hideDateTimePicker();
+  };
+
   render() {
     return (
       <View>
@@ -34,42 +55,74 @@ class TodoCreaterCompoent extends React.Component<{
           maskClosable={false}
           onClose={this.onClose}
           closable={false}
-          style={{ top: -180 }}
+          style={{ top: -130, width: '90%', paddingLeft: 30, paddingRight: 30, paddingBottom: 20 }}
           title={null}
           footer={null}
           wrapProps={{ onTouchStart: this.onWrapTouchStart }}
         >
+          <TouchableOpacity onPress={this.onClose}>
+            <Image
+              style={{ width: 20, height: 20, position: 'absolute', right: -25, top: -5 }}
+              source={require('../../assets/icons/x.png')}
+            />
+          </TouchableOpacity>
+
           <TextInput
             placeholder="您希望做什么？"
             style={{
+              marginTop: 25,
               height: 40,
-              borderColor: 'gray',
-              borderBottomWidth: 1
+              borderColor: '#dadada',
+              borderBottomWidth: 0.8,
+              textAlign: 'center',
+              fontWeight: '800',
+              fontSize: 18
             }}
             onChangeText={text => this.setState({ text })}
             value={this.state.text}
           />
 
-          <List>
-            <Item>
+          <List
+            style={{
+              borderBottomWidth: 0
+            }}
+            styles={{
+              Body: {
+                borderBottomWidth: 0,
+                borderTopWidth: 0
+              }
+            }}
+          >
+            <Item
+              style={{
+                marginTop: 20,
+                marginLeft: 0,
+                paddingLeft: 0,
+                borderBottomWidth: 0.8,
+                borderColor: '#dadada'
+              }}
+            >
               <Flex>
-                <Ionicons name="ios-time-outline" size={25} />
+                <Image
+                  style={{ width: 20, height: 20 }}
+                  source={require('../../assets/icons/clock.png')}
+                />
 
                 <TouchableOpacity
                   style={{ marginLeft: 10, width: '100%' }}
                   onPress={this.showDateTimePicker}
                 >
-                  <Text style={{ color: '#cdcdd1', fontSize: 16 }}>
-                    {this.props.todo.deadline
-                      ? format(this.props.todo.deadline, 'YYYY/MM/dd HH:mm')
-                      : '请选择'}
+                  <Text style={{ color: '#9b9b9b', fontSize: 16 }}>
+                    {this.state.deadline
+                      ? format(this.state.deadline, 'YYYY/MM/dd HH:mm')
+                      : '任务deadline'}
                   </Text>
                 </TouchableOpacity>
               </Flex>
 
               <DateTimePicker
                 mode="datetime"
-                date={this.props.todo.deadline ? new Date(this.props.todo.deadline) : new Date()}
+                date={this.state.deadline ? new Date(this.state.deadline) : new Date()}
                 isVisible={this.state.isDateTimePickerVisible}
                 onConfirm={this.handleDatePicked}
                 onCancel={this.hideDateTimePicker}
