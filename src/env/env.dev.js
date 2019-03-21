bc9dc99583e1b3c543aab3742a9390cc4404b249
fwchen <me@chenfangwei.com>
2019-03-21 20:38:36 +0800

    change address

--- src/env/env.dev.js
+++ src/env/env.dev.js
@@ -1,2 +1,2 @@
-export const API_BASE = 'http://192.168.50.10:8888';
-export const WEBSITE = 'http://192.168.50.10:3000';
+export const API_BASE = 'http://192.168.50.77:8000';
+export const WEBSITE = 'http://192.168.50.77:3000';
